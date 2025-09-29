e0a5fb0a1387a199cb529c9c504a5f446f161ff5
wayne540 <197884612+wayne540@users.noreply.github.com>
2025-09-29 12:17:20 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+// frontend/src/index.js
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Find the root HTML element to mount the React app
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// Render your App.jsx inside that root element
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
